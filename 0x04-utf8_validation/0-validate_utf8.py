acc7771f0fcff6a4084972cf37ffb04e3d4cb9f8
GODSPE1 <godspy18@gmail.com>
2024-07-26 03:51:00 +0100

    validate UTF-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+def validUTF8(data):
+    """Number of bytes in the current UTF-8 character"""
+    num_bytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+    return num_bytes == 0
